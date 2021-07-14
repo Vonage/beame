b808668783f1e64574a19b793bd0a59ce1c5c78a
Tal Weinfeld <tal.weinfeld@vonage.com>
2021-07-14 14:03:35 +0300

    build

--- src/down.js
+++ src/down.js
@@ -63,10 +63,7 @@ module.exports = function({
         })
         .map((buffer)=> ({ url, buffer }));
     })
-    .onValue(({ buffer, url })=> {
-      console.log('writing', url);
-      inStream.write(buffer);
-    })
+    .onValue(({ buffer, url })=> inStream.write(buffer))
     .onError((err)=> inStream.emit('error', err))
     .onEnd(()=> inStream.end());
   
